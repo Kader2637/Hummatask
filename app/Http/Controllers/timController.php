e239f52c417074f609e3854387a5bd50d9bc41b3
Yapidev <88489723+Yapidev@users.noreply.github.com>
2023-10-28 22:49:59 +0700

    fix(validation): tambahan validasi max char

--- app/Http/Controllers/timController.php
+++ app/Http/Controllers/timController.php
@@ -11,68 +11,65 @@ class timController extends Controller
     protected function boardPage($uuid)
     {
         $title = "Tim/board";
-        $tim = Tim::where('uuid',$uuid)->first();
+        $tim = Tim::where('uuid', $uuid)->first();
         $anggota = $tim->user()->get();
 
-
-
-
-        return view('siswa.tim.board', compact('title','tim','anggota'));
+        return view('siswa.tim.board', compact('title', 'tim', 'anggota'));
     }
 
     protected function kalenderPage($uuid)
     {
-        $tim = Tim::where('uuid',$uuid)->first();
+        $tim = Tim::where('uuid', $uuid)->first();
         $anggota = $tim->user()->get();
 
         $title = "Tim/kalender";
-        return view('siswa.tim.kalender', compact('title','tim','anggota'));
+        return view('siswa.tim.kalender', compact('title', 'tim', 'anggota'));
     }
 
     protected function projectPage($uuid)
     {
-        $tim = Tim::where('uuid',$uuid)->first();
+        $tim = Tim::where('uuid', $uuid)->first();
         $anggota = $tim->user()->get();
 
         $title = "Tim/project";
-        return view('siswa.tim.project', compact('title','tim','anggota'));
+        return view('siswa.tim.project', compact('title', 'tim', 'anggota'));
     }
 
     protected function historyPage($uuid)
     {
         $title = "Tim/history";
 
-        $tim = Tim::where('uuid',$uuid)->first();
+        $tim = Tim::where('uuid', $uuid)->first();
         $anggota = $tim->user()->get();
 
 
-        return view('siswa.tim.history', compact('title','tim','anggota'));
+        return view('siswa.tim.history', compact('title', 'tim', 'anggota'));
     }
 
     protected function historyPresentasiPage($uuid)
     {
         $title = "Tim/presentasi";
-        $tim = Tim::where('uuid',$uuid)->first();
+        $tim = Tim::where('uuid', $uuid)->first();
         $anggota = $tim->user()->get();
 
-        return view('siswa.tim.history-presentasi', compact('title','tim','anggota'));
+        return view('siswa.tim.history-presentasi', compact('title', 'tim', 'anggota'));
     }
 
     protected function catatanPage($uuid)
     {
-        $tim = Tim::where('uuid',$uuid)->first();
+        $tim = Tim::where('uuid', $uuid)->first();
         $anggota = $tim->user()->get();
 
         $title = "catatan";
-        return view('siswa.tim.catatan', compact('title','anggota','tim'));
+        return view('siswa.tim.catatan', compact('title', 'anggota', 'tim'));
     }
 
     protected function historyCatatanPage($uuid)
     {
-        $tim = Tim::where('uuid',$uuid)->first();
+        $tim = Tim::where('uuid', $uuid)->first();
         $anggota = $tim->user()->get();
 
         $title = "catatan history";
-        return view('siswa.tim.history-catatan', compact('title','anggota','tim'));
+        return view('siswa.tim.history-catatan', compact('title', 'anggota', 'tim'));
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -147,25 +147,29 @@
                     </li>
                     <li class="menu-item">
                         <ul class="">
-                            @forelse ( $tims as $tim )
-
-                            <li class="menu-item ">
-                                <a href="{{ route('tim.board',$tim->uuid) }}" class="menu-link d-flex align-items-center gap-2">
-                                    <img style="width: 30px;height:30px;object-fit: cover" class="rounded-circle border border-primary"
-                                        src="{{ asset( 'storage/' .$tim->logo ) }}" alt="">
-                                    <div class="">{{ $tim->nama }}</div>
-                                </a>
-                            </li>
+                            @forelse ($tims as $tim)
+                                <li class="menu-item ">
+                                    <a href="{{ route('tim.board', $tim->uuid) }}"
+                                        class="menu-link d-flex align-items-center gap-2">
+                                        <img style="width: 30px;height:30px;object-fit: cover"
+                                            class="rounded-circle border border-primary"
+                                            src="{{ asset('storage/' . $tim->logo) }}" alt="">
+                                        <div class="">{{ $tim->nama }}</div>
+                                    </a>
+                                </li>
 
                             @empty
 
-                            <li class="menu-item bg-info bg-light ">
-                                <a class="menu-link d-flex align-items-center gap-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 512 512"><path fill="currentColor" d="M256 16c-48 0-80 32-80 64c0 48 16 80 32 96v16h96v-16c16-16 32-48 32-96c0-32-32-64-80-64zm-63.6 65.33L256 102.5l63.6-21.17l-39.2 97.97l-16.8-6.6l24.8-62l-32.4 10.8l-32.4-10.8l24.8 62l-16.8 6.6l-39.2-97.97zm-83.3 79.07c-23.4 3-44.6 30.5-44.6 65.9c0 19.6 6.8 36.9 16.7 48.9l11.9 14.2l-18.3 3.4c-12.9 2.5-22.3 9.3-30.4 20.4c-8.1 11.1-14.3 26.5-18.6 44.4C18 389.8 16.2 429.2 16 464h42.8l2.24 30H169.6l2-30h40.8c0-35.2-.4-75.1-7.5-107.7c-4-17.9-9.9-33.3-18.1-44.3c-8.2-11-18.1-17.8-32.6-20l-18.5-2.9l11.7-14.7c9.5-11.9 15.9-29 15.9-48.1c0-37.8-23.6-65.8-49.4-65.8l-4.8-.1zm283.6 0c-23.4 3-44.6 30.5-44.6 65.9c0 19.6 6.8 36.9 16.7 48.9l11.9 14.2l-18.3 3.4c-12.9 2.5-22.3 9.3-30.4 20.4c-8.1 11.1-14.3 26.5-18.6 44.4c-7.8 32.2-9.6 71.6-9.8 106.4h42.8l2.2 30h108.6l2-30H496c0-35.2-.4-75.1-7.5-107.7c-4-17.9-9.9-33.3-18.1-44.3c-8.2-11-18.1-17.8-32.6-20l-18.5-2.9l11.7-14.7c9.5-11.9 15.9-29 15.9-48.1c0-37.8-23.6-65.8-49.4-65.8l-4.8-.1zM208 209v18h96v-18h-96zm16 34v18h64v-18h-64z"/></svg>
-                                    <div class="">Belum punya tim</div>
-                                </a>
-                            </li>
-
+                                <li class="menu-item bg-info bg-light ">
+                                    <a class="menu-link d-flex align-items-center gap-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+                                            viewBox="0 0 512 512">
+                                            <path fill="currentColor"
+                                                d="M256 16c-48 0-80 32-80 64c0 48 16 80 32 96v16h96v-16c16-16 32-48 32-96c0-32-32-64-80-64zm-63.6 65.33L256 102.5l63.6-21.17l-39.2 97.97l-16.8-6.6l24.8-62l-32.4 10.8l-32.4-10.8l24.8 62l-16.8 6.6l-39.2-97.97zm-83.3 79.07c-23.4 3-44.6 30.5-44.6 65.9c0 19.6 6.8 36.9 16.7 48.9l11.9 14.2l-18.3 3.4c-12.9 2.5-22.3 9.3-30.4 20.4c-8.1 11.1-14.3 26.5-18.6 44.4C18 389.8 16.2 429.2 16 464h42.8l2.24 30H169.6l2-30h40.8c0-35.2-.4-75.1-7.5-107.7c-4-17.9-9.9-33.3-18.1-44.3c-8.2-11-18.1-17.8-32.6-20l-18.5-2.9l11.7-14.7c9.5-11.9 15.9-29 15.9-48.1c0-37.8-23.6-65.8-49.4-65.8l-4.8-.1zm283.6 0c-23.4 3-44.6 30.5-44.6 65.9c0 19.6 6.8 36.9 16.7 48.9l11.9 14.2l-18.3 3.4c-12.9 2.5-22.3 9.3-30.4 20.4c-8.1 11.1-14.3 26.5-18.6 44.4c-7.8 32.2-9.6 71.6-9.8 106.4h42.8l2.2 30h108.6l2-30H496c0-35.2-.4-75.1-7.5-107.7c-4-17.9-9.9-33.3-18.1-44.3c-8.2-11-18.1-17.8-32.6-20l-18.5-2.9l11.7-14.7c9.5-11.9 15.9-29 15.9-48.1c0-37.8-23.6-65.8-49.4-65.8l-4.8-.1zM208 209v18h96v-18h-96zm16 34v18h64v-18h-64z" />
+                                        </svg>
+                                        <div class="">Belum punya tim</div>
+                                    </a>
+                                </li>
                             @endforelse
                         </ul>
                     </li>
@@ -579,7 +583,8 @@
                             const logoInput = document.querySelector('input[name="logo"]');
 
                             // Validasi input kosong
-                            if (namaInput.value.trim() === '' || logoInput.files.length === 0) {
+                            if (namaInput.value.trim() === '' || repositoryInput.value.trim() === '' || logoInput.files
+                                .length === 0) {
                                 event.preventDefault(); // Mencegah pengiriman formulir
                                 Swal.fire({
                                     icon: 'warning',
@@ -608,6 +613,22 @@
                                         icon: 'warning',
                                         title: 'Peringatan',
                                         text: 'File yang diunggah harus berupa gambar (jpeg, jpg, png, atau gif)!',
+                                    });
+                                }
+                                // Validasi panjang maksimum untuk namaInput dan repositoryInput
+                                else if (namaInput.value.length > 50) {
+                                    event.preventDefault(); // Mencegah pengiriman formulir
+                                    Swal.fire({
+                                        icon: 'warning',
+                                        title: 'Peringatan',
+                                        text: 'Nama Tim harus kurang dari atau sama dengan 50 karakter!',
+                                    });
+                                } else if (repositoryInput.value.length > 100) {
+                                    event.preventDefault(); // Mencegah pengiriman formulir
+                                    Swal.fire({
+                                        icon: 'warning',
+                                        title: 'Peringatan',
+                                        text: 'URL Repository harus kurang dari atau sama dengan 100 karakter!',
                                     });
                                 }
                             }
