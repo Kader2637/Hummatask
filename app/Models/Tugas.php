818d77eca93c75c861970f8ebcd67940ff410ebf
KaitoDeCode <kurniawanadi4556@gmail.com>
2023-10-30 00:07:08 +0700

    Revert "feat ( autoIncrement ) => AI UUID in Model"
    This reverts commit b09ea368791ca8d5cde7c7b268ef52c97e9ca064.

--- app/Models/Tugas.php
+++ app/Models/Tugas.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Support\Str;
 
 class Tugas extends Model
 {
@@ -19,46 +18,4 @@ class Tugas extends Model
     {
         return $this->belongsTo(Tim::class);
     }
-
-     /**
-     * Indicates if the IDs are auto-incrementing.
-     *
-     * @var bool
-     */
-    public $incrementing = false;
-
-    /**
-     * The "booting" method of the model.
-     *
-     * @return void
-     */
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::creating(function ($model) {
-            if (!$model->getKey()) {
-                $model->{$model->getKeyName()} = (string) Str::uuid();
-            }
-        });
-    }
-
-    /**
-     * Get the primary key type.
-     *
-     * @return string
-     */
-    protected $keyType = 'string';
-
-    /**
-     * Get the key name for the model.
-     *
-     * @return string
-     */
-    public function getKeyName()
-    {
-        return 'id';
-    }
-
-
 }
