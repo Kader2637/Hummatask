84ab43201629ff781ed3868b1f7a7a49260fa4fa
21jefri <140050277+21jefri@users.noreply.github.com>
2023-11-07 14:45:53 +0700

    seeder

--- database/migrations/2023_10_26_031835_create_tims_table.php
+++ database/migrations/2023_10_26_031835_create_tims_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('logo')->default('avatar');
             $table->string('nama')->nullable();
             $table->string('repository')->nullable();
-            $table->enum('status_tim',['solo','pre_mini','mini','pre_big','big']);
+            $table->enum('status_tim',['solo','pre_mini','mini','big']);
             $table->boolean('sudah_presentasi')->default(false);
             $table->boolean('kadaluwarsa')->default(false);
             $table->timestamps();
