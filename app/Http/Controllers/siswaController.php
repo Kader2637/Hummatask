db80bea341f714709615a8a532397685a56072da
21jefri <140050277+21jefri@users.noreply.github.com>
2023-11-16 13:26:34 +0700

    Dashboard ->user

--- app/Http/Controllers/siswaController.php
+++ app/Http/Controllers/siswaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Anggota;
 use App\Models\Notifikasi;
 use App\Models\User;
+use App\Models\Tim;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -17,7 +18,24 @@ class siswaController extends Controller
         $tims = User::find(Auth::user()->id)->tim()->get();
         $userID = Auth::user()->id;
         $notifikasi = Notifikasi::where('user_id', $userID)->get();
-        return response()->view('siswa.dashboard', compact('title','tims','notifikasi'));
+
+        $tugas = User::find(Auth::user()->id)->tugas()->where('status_tugas', 'tugas_baru')->get()
+            ->sortBy(function ($item) {
+                $deadline = \Carbon\Carbon::parse($item->deadline);
+                $created = \Carbon\Carbon::parse($item->created_at);
+                return $deadline->diffInDays($created);
+            });
+        $tugasBelum = User::find(Auth::user()->id)->tugas()->where(function ($query) {
+            $query->where('status_tugas', 'revisi')
+                ->OrWhere('status_tugas', 'dikerjakan');
+        })->get()
+            ->sortBy(function ($item) {
+                $deadline = \Carbon\Carbon::parse($item->deadline);
+                $created = \Carbon\Carbon::parse($item->created_at);
+                return $deadline->diffInDays($created);
+            });
+
+        return response()->view('siswa.dashboard', compact('title', 'tims', 'tugas', 'tugasBelum', 'notifikasi'));
     }
 
     // Return view profile siswa
@@ -26,9 +44,6 @@ class siswaController extends Controller
         $title = 'Profile Siswa';
         $user = User::with('peran')->where('id', Auth::user()->id)->first();
         $tims = $user->tim()->get();
-        // dd($tims);
-        
- 
         return response()->view('siswa.profile-siswa', compact('title', 'user', 'tims'));
     }
 }
--- resources/views/siswa/dashboard.blade.php
+++ resources/views/siswa/dashboard.blade.php
@@ -25,30 +25,38 @@
     <main class="container-fluid p-3">
         <div class="row">
             <div class=" jumbotron col-12 d-flex flex-column align-items-center justify-content-center">
-                <p>{{ \Carbon\Carbon::now()->translatedFormat('l, j F Y') }}</p>
+                <p class="fs-5">{{ \Carbon\Carbon::now()->translatedFormat('l, j F Y') }}</p>
                 <p class="fs-3">Selamat datang, {{ Auth::user()->username }}</p>
             </div>
         </div>
-
         <div class="row">
-            <div class="col-lg-4">
-                <div class="kanban-title-board fs-5">Tugas yang belum selesai</div>
-            </div>
+            @if (count($tugasBelum) > 0 || count($tugas) > 0)
+                <div class="col-lg-4">
+                    <div class="kanban-title-board fs-5 fw-bold">Tugas yang belum selesai</div>
+                </div>
         </div>
-        <div class="row mt-4 justify-content-center mb-2" id="navs-tab-sesijalani">
-            @for ($i = 0; $i < 3; $i++)
+        <div class="row mt-4 justify-content-start mb-2" id="navs-tab-sesijalani">
+            @php $count = 0; @endphp
+            @foreach ($tugasBelum as $data)
                 <div class="col-lg-4">
                     <div class="card text-center mb-3">
                         <div class="kanban-item" data-eid="in-progress-1" data-comments="12" data-badge-text="UX"
                             data-badge="success" data-due-date="5 April" data-attachments="4" data-assigned="12.png,5.png"
                             data-members="Bruce,Clark">
-                            <div class="d-flex justify-content-between flex-wrap align-items-center mb-2 pb-1">
+                            <div class="d-flex justify-content-start flex-wrap align-items-center mb-2 pb-1">
                                 <div class="item-badges">
-                                    <div class="badge rounded-pill bg-label-success"> UX</div>
+                                    <div class="badge rounded-pill bg-label-success">{{ $data->prioritas }}</div>
+                                </div>
+                                @php
+                                    $deadline = \Carbon\Carbon::parse($data->deadline);
+                                    $created = \Carbon\Carbon::parse($data->created_at);
+                                    $days = $deadline->diffInDays($created);
+                                @endphp
+                                <div style="font-size: 12px; margin-left: 10px;">
+                                    {{ $days }} hari lagi
                                 </div>
                             </div>
-                            <span class="kanban-text text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex,
-                                impedit!</span>
+                            <span class="kanban-text text-left">{{ $data->nama }}</span>
                             <div class="d-flex justify-content-between align-items-center flex-wrap mt-2 pt-1">
                                 <div class="d-flex"> <span class="d-flex align-items-center me-2"><i
                                             class="ti ti-paperclip ti-xs me-1"></i><span class="attachments">4</span></span>
@@ -69,27 +77,39 @@
                         </div>
                     </div>
                 </div>
-            @endfor
+                @php $count++; @endphp
+                @if ($count % 3 === 0)
+                    <div class="row mt-4 justify-content-between mb-2" id="navs-tab-sesijalani">
+                @endif
+            @endforeach
         </div>
         <div class="row">
             <div class="col-lg-4">
-                <div class="kanban-title-board fs-5">Tugas terbaru</div>
+                <div class="kanban-title-board fs-5 fw-bold">Tugas terbaru</div>
             </div>
         </div>
-        <div class="row mt-4 justify-content-center" id="navs-tab-sesijalani">
-            @for ($i = 0; $i < 3; $i++)
+        <div class="row mt-4 d-flex justify-content-start" id="navs-tab-sesijalani">
+            @php $count2 = 0; @endphp
+            @foreach ($tugas as $item)
                 <div class="col-lg-4">
                     <div class="card text-center mb-3">
                         <div class="kanban-item" data-eid="in-progress-1" data-comments="12" data-badge-text="UX"
                             data-badge="success" data-due-date="5 April" data-attachments="4" data-assigned="12.png,5.png"
                             data-members="Bruce,Clark">
-                            <div class="d-flex justify-content-between flex-wrap align-items-center mb-2 pb-1">
+                            <div class="d-flex justify-content-start flex-wrap align-items-center mb-2 pb-1">
                                 <div class="item-badges">
-                                    <div class="badge rounded-pill bg-label-success"> UX</div>
+                                    <div class="badge rounded-pill bg-label-success">{{ $item->prioritas }}</div>
+                                </div>
+                                @php
+                                    $deadline = \Carbon\Carbon::parse($item->deadline);
+                                    $created = \Carbon\Carbon::parse($item->created_at);
+                                    $days = $deadline->diffInDays($created);
+                                @endphp
+                                <div style="font-size:12px; margin-left:10px;">
+                                    {{ $days }} hari lagi
                                 </div>
                             </div>
-                            <span class="kanban-text text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex,
-                                impedit!</span>
+                            <span class="kanban-text text-left">{{ $item->nama }}</span>
                             <div class="d-flex justify-content-between align-items-center flex-wrap mt-2 pt-1">
                                 <div class="d-flex"> <span class="d-flex align-items-center me-2"><i
                                             class="ti ti-paperclip ti-xs me-1"></i><span class="attachments">4</span></span>
@@ -110,8 +130,22 @@
                         </div>
                     </div>
                 </div>
-            @endfor
+                @php $count2++; @endphp
+                @if ($count2 % 3 === 0)
         </div>
+        <div class="row mt-4 d-flex justify-content-between" id="navs-tab-sesijalani">
+            @endif
+            @endforeach
+        </div>
+    @else
+        <div class="row">
+            <p class="text-center fw-classic fs-5">Data masih kosong</p>
+            <div class="col-lg-12 d-flex justify-content-center">
+                <img src="{{ asset('assets/img/illustrations/page-misc-under-maintenance.png') }}"
+                    alt="page-misc-under-maintenance" width="300" class="img-fluid">
+            </div>
+        </div>
+        @endif
     </main>
 @endsection
 
