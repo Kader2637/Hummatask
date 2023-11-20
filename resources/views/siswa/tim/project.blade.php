60ac4a6f4c3b926c4dd63a73ab8078443205e919
KaitoDeCode <kurniawanadi4556@gmail.com>
2023-11-20 23:11:07 +0700

    fix ( revisi ) => feedback

--- resources/views/siswa/tim/project.blade.php
+++ resources/views/siswa/tim/project.blade.php
@@ -180,11 +180,7 @@
                                 data-bs-target="#navs-pills-top-profile" aria-controls="navs-pills-top-profile"
                                 aria-selected="false" data-tab="2">Anggota</button>
                         </div>
-                        <div class="nav-item button-nav" role="tab">
-                            <button type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasEnd"
-                                class="nav-link"><i class="menu-icon tf-icons ti ti-chart-line"></i>
-                                Statistik Project</button>
-                        </div>
+                    
                     </div>
                     <div class="" role="presentation">
                         @auth
@@ -203,8 +199,8 @@
                             @endif
                         @endauth
                     </div>
-                    
-                    
+
+
                 </div>
                 <div class="tab-content bg-transparent pb-0" style="box-shadow: none;">
                     <div class="tab-pane fade active show" id="navs-pills-top-home" role="tabpanel">
