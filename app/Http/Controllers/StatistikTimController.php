9e13c9cda97b1feac263d6c0ca0b3cec01382c17
KaitoDeCode <kurniawanadi4556@gmail.com>
2023-11-22 10:28:00 +0700

    feat( views ) => statistik

--- app/Http/Controllers/StatistikTimController.php
+++ app/Http/Controllers/StatistikTimController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Tim;
 use App\Models\User;
+use Exception;
 
 
 class StatistikTimController extends Controller
@@ -37,5 +38,35 @@ class StatistikTimController extends Controller
         ];
 
         return response()->json($tugas);
+    }
+
+    protected function getProgres($codeTim){
+
+            $tugas = Tim::where('code',$codeTim)->first()->tugas;
+
+        
+            $tugas_baru = $tugas->where('status_tugas','tugas_baru')->count();
+            $tugas_dikerjakan = $tugas->where('status_tugas','dikerjakan')->count();
+            $tugas_selesai = $tugas->where('status_tugas','selesai')->count();
+            $tugas_direvisi = $tugas->where('status_tugas','revisi')->count();
+
+            $presentase_tugas_baru = $tugas_baru !== 0 ? ($tugas_baru / $tugas->count())*100 : 0;
+            $presentase_tugas_dikerjakan = $tugas_dikerjakan !== 0 ? ($tugas_dikerjakan / $tugas->count())*100 : 0;
+            $presentase_tugas_selesai = $tugas_selesai !== 0 ? ($tugas_selesai / $tugas->count())*100 : 0;
+            $presentase_tugas_direvisi = $tugas_direvisi !== 0 ? ($tugas_direvisi /$tugas->count())*100 : 0;
+
+
+            return response()->json([
+                "tugas_baru" => $tugas_baru,
+                "tugas_direvisi" => $tugas_direvisi,
+                "tugas_selesai" => $tugas_selesai,
+                "tugas_dikerjakan" => $tugas_dikerjakan,
+                "persentase_tugas_baru"=> $presentase_tugas_baru,
+                "persentase_tugas_dikerjakan" => $presentase_tugas_dikerjakan,
+                "persentase_tugas_selesai" => $presentase_tugas_selesai,
+                "persentase_tugas_direvisi" => $presentase_tugas_direvisi,
+            ]);
+
+
     }
 }
