b1a4a8f3aa6c115528147da420da882da93090bf
aneexx <andirizki1305@gmail.com>
2023-11-29 10:19:07 +0700

    Update count notif

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -650,7 +650,12 @@
 
                 daftarNotifikasi.empty();
 
-                countBadge.text(notifikasi.length);
+                if (notifikasi.length > 0) {
+                    countBadge.text(notifikasi.length);
+                } else {
+                    countBadge
+                .hide();
+                }
 
                 notifikasi.reverse();
 
