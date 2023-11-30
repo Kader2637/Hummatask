e05071395fbf9dd6821c0a16aa834b998c4bd861
KaitoDeCode <kurniawanadi4556@gmail.com>
2023-11-30 08:09:23 +0700

    presentasi ketua magang

--- resources/views/mentor/presentasi.blade.php
+++ resources/views/mentor/presentasi.blade.php
@@ -387,7 +387,6 @@
     </div>
 @endsection
 
-
 @section('script')
     <script src="{{ asset('assets/js/forms-selects.js') }}"></script>
     <script src="{{ asset('assets/vendor/libs/select2/select2.js') }}"></script>
