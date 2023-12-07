1f8cd69f2427a417de4632e968ddf2c47a3d606c
Yapidev <88489723+Yapidev@users.noreply.github.com>
2023-12-07 08:13:50 +0700

    modal

--- resources/views/ketuaMagang/project.blade.php
+++ resources/views/ketuaMagang/project.blade.php
@@ -232,7 +232,7 @@
 
         {{-- Modal detail --}}
         <div class="modal fade" id="modalDetail" tabindex="-1" aria-hidden="true">
-            <div class="modal-dialog modal-fullscreen modal-dialog-centered" role="document">
+            <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="modalCenterTitle">Detail tim</h5>
