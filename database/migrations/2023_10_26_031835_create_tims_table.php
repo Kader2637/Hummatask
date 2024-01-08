a349dc9d65f0f5ab9093624a3c3722d1f4a3532d
Yapidev <88489723+Yapidev@users.noreply.github.com>
2024-01-08 15:21:05 +0700

    updatee

---
+++ database/migrations/2023_10_26_031835_create_tims_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        Schema::create('tims', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->string('logo')->default('avatar');
+            $table->string('nama')->nullable();
+            $table->string('repository')->nullable();
+            $table->foreignId('divisi_id')->constrained('divisis');
+            $table->enum('status_tim',['solo','pre_mini','mini','big']);
+            $table->boolean('sudah_presentasi')->default(false);
+            $table->boolean('kadaluwarsa')->default(false);
+            $table->boolean('status_projek')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tims');
+    }
+};
