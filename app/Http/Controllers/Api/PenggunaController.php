1e14ea43d1abcdff91eb627012ac3b1092a01b7e
Rizki-1 <143168532+Rizki-1@users.noreply.github.com>
2024-01-08 11:40:56 +0700

    tambahan

--- app/Http/Controllers/Api/PenggunaController.php
+++ app/Http/Controllers/Api/PenggunaController.php
@@ -82,13 +82,9 @@ class PenggunaController extends Controller
 
             if (count($errors) > 1) {
                 $errorMessage = implode(', ', $errors);
-                return redirect()->back()
-                    ->with('error', $errorMessage)
-                    ->withInput();
+                return response()->json(['error' => $errorMessage]);
             } else {
-                return redirect()->back()
-                    ->with('error', $errors[0])
-                    ->withInput();
+                return response()->json(['error' => $errors[0]]);
             }
         }
 
@@ -120,7 +116,7 @@ class PenggunaController extends Controller
         } catch (\Exception $e) {
             return redirect()->back()->with('error', 'User gagal disimpan!');
         }
-        return ResponseHelper::success(null, 'Berhasil menambahkan User');
+        return response()->json(['success' => 'Berhasil menambah pengguna']);
     }
 
      /**
