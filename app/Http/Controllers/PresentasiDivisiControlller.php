2bedbd2580a0741b5d2feeb6d9ed215ba8b010e5
Yapidev <88489723+Yapidev@users.noreply.github.com>
2024-01-10 08:55:45 +0700

    Update PresentasiDivisiControlller.php

--- app/Http/Controllers/PresentasiDivisiControlller.php
+++ app/Http/Controllers/PresentasiDivisiControlller.php
@@ -11,7 +11,6 @@ use Illuminate\Http\Request;
 
 class PresentasiDivisiControlller extends Controller
 {
-
     /**
      * store
      *
@@ -22,8 +21,7 @@ class PresentasiDivisiControlller extends Controller
     {
         $data = $request->validated();
         $data['divisi_id'] = auth()->user()->divisi_id;
-        PresentasiDivisi::query()
-            ->updateOrCreate(['divisi_id' => $data['divisi_id'], 'day' => $data['day']], ['limit' => $data['limit']]);
+        PresentasiDivisi::query()->updateOrCreate(['divisi_id' => $data['divisi_id'], 'day' => $data['day']], ['limit' => $data['limit']]);
         // $presentasiDivisi->limitPresentasiDivisis()->delete();
         // foreach ($data['mulai'] as $index => $dari) {
         //     LimitPresentasiDevisi::query()
@@ -33,7 +31,9 @@ class PresentasiDivisiControlller extends Controller
         //             'akhir' => $data['akhir'][$index]
         //         ]);
         // }
-        return redirect()->back()->with('success', 'Berhasil menambahkan limit');
+        return redirect()
+            ->back()
+            ->with('success', 'Berhasil menambahkan limit');
     }
 
     /**
@@ -45,21 +45,38 @@ class PresentasiDivisiControlller extends Controller
     public function createJam(LimitPresentasiDivisiRequest $request): RedirectResponse
     {
         $data = $request->validated();
-        $pDivisi = PresentasiDivisi::query()
-            ->findOrFail($data['presentasi_divisi_id']);
-        $pDivisi->limitPresentasiDivisis()->delete();
-        for ($i = 0; $i < (int) $pDivisi->limit; $i++) {
-            LimitPresentasiDevisi::query()
-                ->create([
-                    'presentasi_divisi_id' => $data['presentasi_divisi_id'],
-                    'mulai' => $data['mulai'][$i],
-                    'akhir' => $data['akhir'][$i]
-                ]);
+
+        $pDivisi = PresentasiDivisi::query()->findOrFail($data['presentasi_divisi_id']);
+        $existingLimitPresentasiIds = $pDivisi->limitPresentasiDivisis()->pluck('id')->toArray();
+
+        $inputCount = min((int) $pDivisi->limit, count($data['mulai']));
+
+        for ($i = 0; $i < $inputCount; $i++) {
+            $limitPresentasi = [
+                'presentasi_divisi_id' => $data['presentasi_divisi_id'],
+                'mulai' => $data['mulai'][$i],
+                'akhir' => $data['akhir'][$i]
+            ];
+
+            if (isset($existingLimitPresentasiIds[$i])) {
+                LimitPresentasiDevisi::query()
+                    ->where('id', $existingLimitPresentasiIds[$i])
+                    ->update($limitPresentasi);
+            } else {
+                LimitPresentasiDevisi::query()->create($limitPresentasi);
+            }
         }
+
+        // Hapus entri yang berlebih jika ada
+        if (count($existingLimitPresentasiIds) > $inputCount) {
+            LimitPresentasiDevisi::query()
+                ->whereIn('id', array_slice($existingLimitPresentasiIds, $inputCount))
+                ->delete();
+        }
+
         return redirect()->back()->with('success', 'Berhasil menambahkan jadwal');
-
     }
-
+    
     /**
      * destroy
      *
@@ -67,8 +84,10 @@ class PresentasiDivisiControlller extends Controller
      * @return RedirectResponse
      */
     public function destroy(PresentasiDivisi $presentasi_divisi): RedirectResponse
-{
-    $presentasi_divisi->delete();
-    return redirect()->back()->with('success', 'Berhasil menghapus divisi');
-}
+    {
+        $presentasi_divisi->delete();
+        return redirect()
+            ->back()
+            ->with('success', 'Berhasil menghapus divisi');
+    }
 }
