452f2f753b4f13bdd890c50e9283d7b19129fa0d
Irsyadandhikaariadi <113318445+Irsyadandhikaariadi@users.noreply.github.com>
2024-02-05 14:30:02 +0700

    edit history presentasi

--- resources/views/mentor/history-presentasi.blade.php
+++ resources/views/mentor/history-presentasi.blade.php
@@ -6,27 +6,20 @@
             <span class="text-muted fw-light"></span> History
         </h4>
         <div class="d-flex card flex-md-row align-items-center justify-content-between">
-            <div class=" nav nav-pills mb-3 mt-3 d-flex flex-wrap navbar-ul px-3" id="pills-tab" role="tablist">
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home"
-                        type="button" role="tab" aria-controls="pills-home" aria-selected="true" data-tab="1"><i
-                            class="fa-solid fa-user-times icon-text me-2"></i>Belum Presentasi Hari Ini</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile"
-                        type="button" role="tab" aria-controls="pills-profile" aria-selected="false" data-tab="2"><i
-                            class="fa-solid fa-person-chalkboard icon-text me-2"></i>Selesai Presentasi Hari Ini</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-present"
-                        type="button" role="tab" aria-controls="pills-present" aria-selected="false" data-tab="2"><i
-                            class="fa-solid Example of calendar-check-o fa-calendar-check-o icon-text me-2"></i>Sudah Presentasi Minggu Ini</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-aduh"
-                        type="button" role="tab" aria-controls="pills-aduh" aria-selected="false" data-tab="2"><i
-                            class="fa-solid fa-calendar-xmark icon-text me-2"></i>Tidak Presentasi Mingguan</button>
-                </li>
+            <div class="nav nav-pills mb-3 mt-3 d-flex flex-wrap navbar-ul px-3" id="pills-tab" role="tablist">
+                <form id="filterForm" action="{{ route('tim') }}" method="get">
+                    <select id="select2Basic" name="status_tim" class="form-select select2" data-allow-clear="true">
+                        <option value="" disabled selected>Pilih Data</option>
+                        <option value="belum_presentasi">Belum Presentasi Hari
+                        </option>
+                        <option value="selesai_presentasi">Selesai Presentasi Hari Ini Project</option>
+                        <option value="sudah_presentasi">Sudah
+                            Presentasi Minggu Ini
+                        </option>
+                        <option value="tidak_presentasi">Tidak Presentasi Mingguan
+                        </option>
+                    </select>
+                </form>
             </div>
         </div>
         <div class="tab-content px-0 mt-2" id="pills-tabContent">
@@ -41,6 +34,7 @@
                                     <th scope="col">NAMA TIM</th>
                                     <th scope="col">STATUS TIM</th>
                                     <th scope="col">HARI/TANGGAL</th>
+                                    <th scope="col">AKSI</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -60,6 +54,9 @@
                                             @endif
                                         </td>
                                         <td>{{ \Carbon\Carbon::now()->translatedFormat('l, j F Y') }}</td>
+                                        <td><button class="btn btn-warning" data-bs-target="#modal" data-bs-toggle="modal"
+                                                data style="padding: 10px;"><i
+                                                    class="fa-solid fa-pen-to-square"></i></button></td>
                                     </tr>
                                 @endforeach
                             </tbody>
@@ -136,57 +133,57 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                    @php
-                                        function terjemahkanHari($namaHari)
-                                        {
-                                            switch ($namaHari) {
-                                                case 'Monday':
-                                                    return 'Senin';
-                                                case 'Tuesday':
-                                                    return 'Selasa';
-                                                case 'Wednesday':
-                                                    return 'Rabu';
-                                                case 'Thursday':
-                                                    return 'Kamis';
-                                                case 'Friday':
-                                                    return 'Jumat';
-                                                default:
-                                                    return $namaHari;
-                                            }
+                                @php
+                                    function terjemahkanHari($namaHari)
+                                    {
+                                        switch ($namaHari) {
+                                            case 'Monday':
+                                                return 'Senin';
+                                            case 'Tuesday':
+                                                return 'Selasa';
+                                            case 'Wednesday':
+                                                return 'Rabu';
+                                            case 'Thursday':
+                                                return 'Kamis';
+                                            case 'Friday':
+                                                return 'Jumat';
+                                            default:
+                                                return $namaHari;
                                         }
+                                    }
 
-                                        function terjemahkanBulan($namaBulan)
-                                        {
-                                            switch ($namaBulan) {
-                                                case 'January':
-                                                    return 'Januari';
-                                                case 'February':
-                                                    return 'Februari';
-                                                case 'March':
-                                                    return 'Maret';
-                                                case 'April':
-                                                    return 'April';
-                                                case 'May':
-                                                    return 'Mei';
-                                                case 'June':
-                                                    return 'Juni';
-                                                case 'July':
-                                                    return 'Juli';
-                                                case 'August':
-                                                    return 'Agustus';
-                                                case 'September':
-                                                    return 'September';
-                                                case 'October':
-                                                    return 'Oktober';
-                                                case 'November':
-                                                    return 'November';
-                                                case 'December':
-                                                    return 'Desember';
-                                                default:
-                                                    return $namaBulan;
-                                            }
+                                    function terjemahkanBulan($namaBulan)
+                                    {
+                                        switch ($namaBulan) {
+                                            case 'January':
+                                                return 'Januari';
+                                            case 'February':
+                                                return 'Februari';
+                                            case 'March':
+                                                return 'Maret';
+                                            case 'April':
+                                                return 'April';
+                                            case 'May':
+                                                return 'Mei';
+                                            case 'June':
+                                                return 'Juni';
+                                            case 'July':
+                                                return 'Juli';
+                                            case 'August':
+                                                return 'Agustus';
+                                            case 'September':
+                                                return 'September';
+                                            case 'October':
+                                                return 'Oktober';
+                                            case 'November':
+                                                return 'November';
+                                            case 'December':
+                                                return 'Desember';
+                                            default:
+                                                return $namaBulan;
                                         }
-                                    @endphp
+                                    }
+                                @endphp
                                 @foreach ($presentasiSelesaiMingguan as $item)
                                     <tr>
                                         <td>{{ $loop->iteration }}</td>
@@ -211,30 +208,29 @@
 
                                             {{ $namaHari }}, {{ $tanggal->format('j') }} {{ $namaBulan }}
                                             {{ $tanggal->format('Y') }}
-                                </td>
-                                <td>
-                                    @if ($item->status_presentasi === 'tidak_selesai')
-                                        <span class="badge bg-label-danger">Tidak Selesai</span>
-                                    @elseif ($item->status_presentasi === 'selesai')
-                                        <span class="badge bg-label-success">Selesai</span>
-                                    @endif
-                                </td>
-                                <td>
-                                    @if ($item->status_revisi === 'tidak_selesai')
-                                        <span class="badge bg-label-danger">Tidak Selesai</span>
-                                    @elseif ($item->status_revisi === 'selesai')
-                                        <span class="badge bg-label-success">Selesai</span>
-                                    @endif
-                                </td>
-                                </tr>
+                                        </td>
+                                        <td>
+                                            @if ($item->status_presentasi === 'tidak_selesai')
+                                                <span class="badge bg-label-danger">Tidak Selesai</span>
+                                            @elseif ($item->status_presentasi === 'selesai')
+                                                <span class="badge bg-label-success">Selesai</span>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            @if ($item->status_revisi === 'tidak_selesai')
+                                                <span class="badge bg-label-danger">Tidak Selesai</span>
+                                            @elseif ($item->status_revisi === 'selesai')
+                                                <span class="badge bg-label-success">Selesai</span>
+                                            @endif
+                                        </td>
+                                    </tr>
                                 @endforeach
                             </tbody>
                         </table>
                     </div>
                 </div>
             </div>
-            <div class="tab-pane fade" id="pills-aduh" role="tabpanel" aria-labelledby="pills-profile-tab"
-                tabindex="0">
+            <div class="tab-pane fade" id="pills-aduh" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="0">
                 <div class="card p-3">
                     <div class="card-datatable table-responsive">
                         <table id="jstabel3" class="dt-responsive table">
@@ -279,6 +275,41 @@
                 </div>
             </div>
         </div>
+
+        {{-- Modal Buat Tim --}}
+        {{-- <form action="{{ route('pembuatan.tim') }}" id="createForm" method="post"> --}}
+        @csrf
+        <div class="modal fade" id="modal" tabindex="-1" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modalCenterTitle">Edit Status Presentasi</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col mb-3">
+                                <label for="status_tim" class="form-label">Status Presentasi</label>
+                                <select id="status_tim" name="status_tim" class="select2 form-select form-select-lg"
+                                    data-allow-clear="true">
+                                    <option value="" selected>Presentasi</option>
+                                    <option value="">Tidak Presentasi</option>
+                                </select>
+                                {{-- @error('status_tim')
+                                        <p class="text-danger">{{ $message }}</p>
+                                    @enderror --}}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-label-secondary" data-bs-dismiss="modal">Kembali</button>
+                        <button type="submit" class="btn btn-primary" id="createButton">Simpan</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{-- </form> --}}
+        {{-- Modal Buat Tim --}}
     </div>
 @endsection
 @section('script')
